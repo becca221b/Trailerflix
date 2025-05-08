2a54c44da70473ec645d407677b7225eb7e1d616
becca221b <lrpmqrp@gmail.com>
2025-05-08 16:05:18 -0300

    add async

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const express= require ("express");
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
@@ -11,51 +11,57 @@ const app = express();
 const dataPath = path.resolve(__dirname,process.env.DATA_PATH);
 
 //Lee y parsea el archivo json
-const TRAILERFLIX = (()=>{
+let TRAILERFLIX = [];
+
+async function cargarPelis() {
     try {
-        const fileData = fs.readFileSync(dataPath, 'utf-8');
-        return JSON.parse(fileData);
+        const fileData = await fs.readFile(dataPath, 'utf-8');
+        TRAILERFLIX = JSON.parse(fileData);
     } catch (err) {
         console.error('Error al leer el archivo json', err.message);
-        return [];
+        TRAILERFLIX = [];
     }
-})();
+};
 
+//Middleware para rutas async
 
+const asyncHandler = fn => (req,res,next) =>{
+    Promise.resolve(fn(req, res, next)).catch(next);
+}
 
 //rutas
 app.get('/',(req,res)=>{
     res.send('<h1>Bienvenidos</h1>')
 })
 
-app.get('/catalogo',(req,res)=>{
+app.get('/catalogo', asyncHandler(async (req,res)=>{
     res.json(TRAILERFLIX);
-})
+}));
 
-app.get('/titulo/:title',(req,res)=>{
+app.get('/titulo/:title',asyncHandler(async (req,res)=>{
     const parametro = req.params.title.trim().toLowerCase();
     const resultado = TRAILERFLIX.filter(pelis => pelis.titulo.toLowerCase().includes(parametro));
     //console.log(resultado);
     res.json(resultado);
-})
+}))
 
-app.get('/categoria/:cat',(req,res)=>{
+app.get('/categoria/:cat', async((req,res)=>{
     const parametro = req.params.cat.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     if(parametro==='serie'|| parametro==='pelicula'){
         const resultado = TRAILERFLIX.filter(pelis => pelis.categoria.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")===parametro);  
         res.json(resultado);
     }else{
-        return res.status(404).send(`<h2>No existe la categoría: ${parametro}</h2>`)
+        res.status(404).send(`<h2>No existe la categoría: ${parametro}</h2>`)
     }
-})
+}))
 
-app.get('/reparto/:act',(req,res)=>{
+app.get('/reparto/:act', async((req,res)=>{
     const parametro= req.params.act.trim().toLowerCase();
     const resultado = TRAILERFLIX.filter(pelis=>pelis.reparto.trim().toLowerCase().includes(parametro));
     res.json(resultado.reparto);
     //acá usar map para retorna solo un array con la propiedad “reparto” y la propiedad
     //“titulo” y sus respectivos datos
-})
+}))
 
 app.get('/trailer/:id',(req,res)=>{
     const parametro = parseInt(req.params.id);
